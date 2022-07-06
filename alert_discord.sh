7a2b8a60a7bf5608a80027136e5f28a8c7810922
broxen <paul@broxen.net>
2022-07-05 18:01:57 -0700

    New File

---
+++ alert_discord.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# LinuxGSM alert_discord.sh module
+# Author: Daniel Gibbs
+# Contributors: http://linuxgsm.com/contrib
+# Website: https://linuxgsm.com
+# Description: Sends Discord alert.
+
+functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+json=$(cat <<EOF
+{
+	"username":"Ullr",
+	"avatar_url":"https://raw.githubusercontent.com/broxen/ullrbot/main/Ullr.jpg",
+	"file":"content",
+	"embeds": [{
+		"color": "2067276",
+		"author": {
+			"name": "${alertemoji} ${alertsubject} ${alertemoji}",
+			"icon_url": "https://raw.githubusercontent.com/broxen/ullrbot/main/Ullr.jpg"
+		},
+		"title": "${servername}",
+		"description": "${alertbody} \n More info: ${alerturl}",
+		"url": "",
+		"type": "content",
+		"thumbnail": {},
+		"fields": [
+			{
+				"name": "Game",
+				"value": "${gamename}",
+				"inline": true
+			},
+			{
+				"name": "Server IP",
+				"value": "valheim.broxen.net:${port}",
+				"inline": true
+			}
+		]
+	}]
+}
+EOF
+)
+
+fn_print_dots "Sending Discord alert"
+
+discordsend=$(curl --connect-timeout 10 -sSL -H "Content-Type: application/json" -X POST -d "$(echo -n "${json}" | jq -c .)" "${discordwebhook}")
+
+if [ -n "${discordsend}" ]; then
+	fn_print_fail_nl "Sending Discord alert: ${discordsend}"
+	fn_script_log_fatal "Sending Discord alert: ${discordsend}"
+else
+	fn_print_ok_nl "Sending Discord alert"
+	fn_script_log_pass "Sending Discord alert"
+fi
